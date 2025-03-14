6913fcb4442901d264005421688ecd38a213c24b
Maria Moreira <maria.ecr.moreira1@gmail.com>
2025-03-14 02:32:39 -0300

    feat: using props and default props

--- aula3/aula3/src/App.jsx
+++ aula3/aula3/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Student from "./Student.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	
+	return(
+		<>
+			<Student name="Nina" age={16} isStudent={false}/>
+			<Student name="Luke" age={12} isStudent={false}/>
+			<Student name="Rex" age={14} isStudent={false}/>
+			<Student name="Rafa" age={2} isStudent={false}/>
+			<Student name="Larry"/>
+		</>
+	);
 }
 
 export default App
